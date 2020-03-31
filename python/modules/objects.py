c14136b97a26ca7f87a0e787cb775518c2264a9f
Théo Dumont <theo.dumont@mines-paristech.fr>
2020-03-31 16:31:54 +0200

    Add Parameters class, clean the others

--- python/modules/objects.py
+++ python/modules/objects.py
@@ -2,19 +2,92 @@
 
 import modules.utils as utils
 import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+
+class Parameters(object):
+
+    def __init__(self):
+        """
+        Parameters class constructor.
+        """
+        # Hangar
+        self.hangar_x = 1.25
+        self.hangar_y = 1.25
+        self.hangar_z = 1
+        self.dimensions_hangar = np.array([self.hangar_x,
+                                           self.hangar_y,
+                                           self.hangar_z])
+        # Mobile
+        self.mobile_x = .25
+        self.mobile_y = .25
+        self.mobile_z = .3
+        self.dimensions_mobile = np.array([self.mobile_x,
+                                           self.mobile_y,
+                                           self.mobile_z])
+        # maximal steps authorized
+        self.max_step_x = .1
+        self.max_step_y = .1
+        self.max_step_z = .1
+        self.max_step_alpha = math.radians(10)
+        self.max_step_beta = math.radians(10)
+        self.max_step_gamma = math.radians(10)
+        # array
+        self.max_step = np.array([self.max_step_x, self.max_step_y,
+                                  self.max_step_z, self.max_step_alpha,
+                                  self.max_step_beta, self.max_step_gamma])
+        # display
+        self.display = False
+        self.dim6d = ['x', 'y', 'z', 'alpha', 'beta', 'gamma']
+        self.typedim = ['translation', 'rotation']
+        self.units = ['m', 'rad']
+
+
+    def __str__(self):
+        """
+        Display the parameters.
+        """
+        self.display_step()
+        self.display_hangar()
+        self.display_mobile()
+        return ""
+
+
+    def display_step(self):
+        """Display step parameters."""
+        print("\nSteps:")
+        for dim in range(6):
+            print("{}-{} step: {:.3f} {}".format(self.dim6d[dim],
+                                                 self.typedim[dim//3],
+                                                 self.max_step[dim],
+                                                 self.units[dim//3]))
+
+    def display_hangar(self):
+        """Display hangar parameters."""
+        print("\nHangar:")
+        for dim in range(3):
+            print("{}: {:.3f} m".format(self.dim6d[dim],
+                                        self.dimensions_hangar[dim]))
+
+    def display_mobile(self):
+        """Display mobile parameters."""
+        print("\nMobile:")
+        for dim in range(3):
+            print("{}: {:.3f} m".format(self.dim6d[dim],
+                                        self.dimensions_mobile[dim]))
 
 
 class Object3d(object):
 
-    def __init__(self, name, dimensions, *args, **kwargs):
+    def __init__(self, name, parameters, *args, **kwargs):
         """
         Object3d class constructor.
         """
         super().__init__(*args, **kwargs)
         self._name = name
         self._position = Coord6d([0, 0, 0, 0, 0, 0])
-        self._dimensions = dimensions
-        self.vertices = [Coord3d(vertex) for vertex in utils.pos_to_vertices(self.position, self.dimensions)]
+        # self._dimensions = dimensions
 
     def __str__(self):
         """
@@ -44,9 +117,6 @@ class Object3d(object):
         self.vertices = [Coord3d(vertex) for vertex in utils.pos_to_vertices(self.position, self.dimensions)]
         print('{} has moved!'.format(self.name))
 
-    @property
-    def dimensions(self):
-        return self._dimensions
 
     @property
     def name(self):
@@ -55,20 +125,32 @@ class Object3d(object):
 
 class Mobile(Object3d):
 
-    def __init__(self, dimensions, *args, **kwargs):
+    def __init__(self, parameters, *args, **kwargs):
         """
         Mobile class constructor.
         """
-        super().__init__("Mobile", dimensions, *args, **kwargs)
+        super().__init__("Mobile", parameters, *args, **kwargs)
+        self._dimensions = parameters.dimensions_mobile
+        self.vertices = [Coord3d(vertex) for vertex in utils.pos_to_vertices(self.position, self.dimensions)]
+
+    @property
+    def dimensions(self):
+        return self._dimensions
 
 
 class Hangar(Object3d):
 
-    def __init__(self, dimensions, *args, **kwargs):
+    def __init__(self, parameters, *args, **kwargs):
         """
         Hangar class constructor.
         """
-        super().__init__("Hangar", dimensions, *args, **kwargs)
+        super().__init__("Hangar", parameters, *args, **kwargs)
+        self._dimensions = parameters.dimensions_hangar
+        self.vertices = [Coord3d(vertex) for vertex in utils.pos_to_vertices(self.position, self.dimensions)]
+
+    @property
+    def dimensions(self):
+        return self._dimensions
 
 
 class Coord3d(object):
@@ -143,14 +225,14 @@ class Coord6d(object):
 
 class Trajectory(object):
 
-    def __init__(self, array, max_step, *args, **kwargs):
+    def __init__(self, parameters, array, *args, **kwargs):
         """
         Trajectory class constructor.
         """
         super().__init__(*args, **kwargs)
         self.name = "Initial"
         self.array = array
-        self.max_step = max_step
+        self.max_step = parameters.max_step
 
     def __str__(self):
         """
